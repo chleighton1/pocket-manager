3ffaed836becec5aba1d4394c15661c3ec0e78fc
Charles Leighton <chleighton1@gmail.com>
2024-03-12 17:57:51 -0400

    feat: create drink card

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,37 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <div className="p-10">
+      <Drink />
+    </div>
   );
 }
+
+function Drink() {
+  return (
+    <div className="bg-red-200 text-cyan-800">
+      <h1>Lafayette Place</h1>
+      <div>
+        <h3>Batch</h3>
+        <ul>
+          <li>.75oz Citadelle gin</li>
+          <li>.75oz St Germain</li>
+          <li>.25oz Maraschino</li>
+        </ul>
+      </div>
+      <p>.75oz Lime</p>
+      <p>.75oz Tart Cherry</p>
+      <p>Garnish: Firestick</p>
+      <p>Glass: Nick & Nora</p>
+    </div>
+  );
+}
+
+// 			//  (1.75oz Batch)*
+// .75oz Citadelle gin
+// .75oz St Germain
+// .25oz Maraschino
+// .75oz Lime
+// .5oz Tart Cherry
+// Glass: Nick & Nora
+// Garnish: Firestick
