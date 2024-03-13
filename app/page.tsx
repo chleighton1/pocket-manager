55ed3e0e8d492fce3133265f63ae2bf6768313cf
Charles Leighton <chleighton1@gmail.com>
2024-03-13 19:28:30 -0400

    feat: cocktail cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
   return (
@@ -9,21 +12,55 @@ export default function Home() {
 }
 
 function Drink() {
+  const [activeCard, setActiveCard] = useState(false);
+
+  const handleFlip = () => {
+    setActiveCard(!activeCard);
+  };
+
   return (
-    <div className="bg-red-200 text-cyan-800">
-      <h1>Lafayette Place</h1>
-      <div>
-        <h3>Batch</h3>
-        <ul>
-          <li>.75oz Citadelle gin</li>
-          <li>.75oz St Germain</li>
-          <li>.25oz Maraschino</li>
-        </ul>
+    <div
+      onClick={handleFlip}
+      className={` text-cyan-800 bg-red-200 w-24  rounded h-96 min-w-80 relative card ${
+        activeCard ? "cardFlip" : ""
+      }`}
+    >
+      <div className="front">
+        <div>
+          <Image
+            src={"/cocktails/Layfayette.jpeg"}
+            alt="Image of cocktail"
+            className=""
+            width={384}
+            height={250}
+          />
+        </div>
+        <h1 className="font-bold text-2xl centered mb-4">Lafayette Place</h1>
       </div>
-      <p>.75oz Lime</p>
-      <p>.75oz Tart Cherry</p>
-      <p>Garnish: Firestick</p>
-      <p>Glass: Nick & Nora</p>
+
+      <div className="back absolute p-6 top-0 w-full h-full ">
+        <h1 className="font-bold text-2xl centered mb-4">Lafayette Place</h1>
+
+        <div className="mb-2">
+          <h3>
+            <span className="font-bold mr-1">1.75oz</span> Batch
+          </h3>
+          <ul className="ml-6">
+            <li>.75oz Citadelle gin</li>
+            <li>.75oz St Germain</li>
+            <li>.25oz Maraschino</li>
+          </ul>
+          <p>
+            <span className="font-bold mr-1">.75oz</span> Lime
+          </p>
+          <p>
+            <span className="font-bold mr-1">.75oz</span> Tart Cherry
+          </p>
+        </div>
+
+        <p>Garnish: Firestick</p>
+        <p>Glass: Nick & Nora</p>
+      </div>
     </div>
   );
 }
